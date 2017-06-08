03b1d17395af2af3c7ab739c4bb6f4628f69d427
Joshua Lockerman <j@new-host-6.home>
2017-06-08 16:52:02 -0400

    Clean up most transmutes.

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@
 use std::cell::UnsafeCell;
 use std::fmt;
 use std::marker::PhantomData;
-use std::mem;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{mem, ptr};
+use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
 use std::sync::Arc;
 
 #[derive(Debug)]
@@ -30,7 +30,7 @@ unsafe impl<T: Send> Sync for Ring<T> {}
 impl<T> Ring<T> {
     pub fn new(cap: usize) -> Self {
         let data: Vec<_> = (0..cap).map(|_|
-            DataCell{ unpacked: EpochPtr::null_at(0) })
+            DataCell{ unpacked: AtomicEpochPtr::null_at(0) })
             .collect();
         let data = data.into_boxed_slice();
         Ring {
@@ -106,7 +106,19 @@ where T: PtrSized {
     }
 }
 
-pub unsafe trait PtrSized: Sized {}
+pub unsafe trait PtrSized: Sized {
+    fn to_ptr(self) -> *mut () {
+        unsafe {
+            let ptr = mem::transmute_copy(&self);
+            mem::forget(self);
+            ptr
+        }
+    }
+
+    unsafe fn from_ptr(ptr: *mut ()) -> Self {
+        mem::transmute_copy(&ptr)
+    }
+}
 
 unsafe impl<T> PtrSized for Box<T> {}
 unsafe impl<T> PtrSized for Option<Box<T>> {}
@@ -116,7 +128,7 @@ unsafe impl<'a, T> PtrSized for &'a mut T {}
 unsafe impl<'a, T> PtrSized for &'a T {}
 
 union DataCell<T> {
-    unpacked: EpochPtr,
+    unpacked: AtomicEpochPtr,
     packed: AtomicU128,
     phantom: PhantomData<T>,
 }
@@ -126,7 +138,7 @@ impl<T> DataCell<T> {
     where T: PtrSized {
         unsafe {
             let cell_val = self.unpacked.load();
-            let is_empty = cell_val.ptr.load(Ordering::Relaxed) == 0;
+            let is_empty = cell_val.ptr.is_null();
             let cell_val = cell_val.to_u128();
             let next_epoch = if is_empty { load_epoch } else { load_epoch + 1 };
             let replacement = EpochPtr::null_at(next_epoch).to_u128();
@@ -134,26 +146,23 @@ impl<T> DataCell<T> {
             match res {
                 Ok(..) if is_empty => { Ok(None) },
                 Ok(packed) => {
-                    let (_, t): (usize, usize) = mem::transmute(packed);
-                    if t == 0 { return Ok(None) }
-                    let t: T = mem::transmute_copy(&t);
-                    Ok(Some(t))
+                    let ptr = EpochPtr::from_u128(packed).ptr;
+                    if ptr.is_null() { return Ok(None) }
+                    Ok(Some(PtrSized::from_ptr(ptr)))
                 },
                 Err(curr_val) => {
-                    let (mut epoch, val): (usize, usize) = mem::transmute(curr_val);
+                    let EpochPtr{mut epoch, ptr} = EpochPtr::from_u128(curr_val);
                     if epoch == load_epoch {
-                        if val == 0 { return Ok(None) }
+                        if ptr.is_null() { return Ok(None) }
                         let res = self.packed.compare_exchange(curr_val, replacement);
                         match res {
                             Ok(packed) => {
-                                let (_, t): (usize, usize) = mem::transmute(packed);
-                                if t == 0 { return Ok(None) }
-                                let t: T = mem::transmute_copy(&t);
-                                return Ok(Some(t))
+                                let ptr = EpochPtr::from_u128(packed).ptr;
+                                if ptr.is_null() { return Ok(None) }
+                                return Ok(Some(PtrSized::from_ptr(ptr)))
                             },
                             Err(packed) => {
-                                let (e, _): (usize, usize) = mem::transmute(packed);
-                                epoch = e
+                                epoch = EpochPtr::from_u128(packed).epoch
                             }
                         }
                     }
@@ -163,23 +172,20 @@ impl<T> DataCell<T> {
         }
     }
 
-    fn put_at_epoch(&self, store_epoch: usize, data: T) -> Option<(T, bool)> {
+    fn put_at_epoch(&self, store_epoch: usize, data: T) -> Option<(T, bool)>
+    where T: PtrSized {
         unsafe {
             let cell_val = EpochPtr::null_at(store_epoch);
             let cell_val = cell_val.to_u128();
-            let data = {
-                let u = mem::transmute_copy(&data);
-                mem::forget(data);
-                u
-            };
+            let data = data.to_ptr();
             let replacement = EpochPtr::val(store_epoch, data);
             let replacement = replacement.to_u128();
             let res = self.packed.compare_exchange(cell_val, replacement);
             match res {
                 Ok(..) => None,
                 Err(packed) => {
-                    let (epoch, _): (usize, usize) = mem::transmute_copy(&packed);
-                    Some((mem::transmute_copy(&data), epoch > store_epoch))
+                    let epoch = EpochPtr::from_u128(packed).epoch;
+                    Some((PtrSized::from_ptr(data), epoch > store_epoch))
                 }
             }
         }
@@ -194,28 +200,45 @@ impl<T> fmt::Debug for DataCell<T> {
 
 #[derive(Debug)]
 #[repr(C)]
-struct EpochPtr {
+struct AtomicEpochPtr {
     epoch: AtomicUsize,
-    ptr: AtomicUsize,
+    ptr: AtomicPtr<()>,
 }
 
-impl EpochPtr {
-    fn load(&self) -> Self {
-        let epoch = AtomicUsize::new(self.epoch.load(Ordering::Relaxed));
-        let ptr = AtomicUsize::new(self.ptr.load(Ordering::Relaxed));
+#[derive(Debug)]
+#[repr(C)]
+struct EpochPtr {
+    epoch: usize,
+    ptr: *mut (),
+}
+
+impl AtomicEpochPtr {
+    fn load(&self) -> EpochPtr {
+        let epoch = self.epoch.load(Ordering::Relaxed);
+        let ptr = self.ptr.load(Ordering::Relaxed);
         EpochPtr{epoch, ptr}
     }
 
+    fn null_at(epoch: usize) -> Self {
+        AtomicEpochPtr{epoch: AtomicUsize::new(epoch), ptr: AtomicPtr::new(ptr::null_mut())}
+    }
+}
+
+impl EpochPtr {
     fn to_u128(self) -> u128 {
         unsafe { mem::transmute(self) }
     }
 
-    fn null_at(epoch: usize) -> Self {
-        EpochPtr{epoch: AtomicUsize::new(epoch), ptr: AtomicUsize::new(0)}
+    fn from_u128(packed: u128) -> Self {
+        unsafe { mem::transmute(packed) }
     }
 
-    fn val(epoch: usize, val: usize) -> Self {
-        EpochPtr{epoch: AtomicUsize::new(epoch), ptr: AtomicUsize::new(val)}
+    fn null_at(epoch: usize) -> Self {
+        EpochPtr{epoch, ptr: ptr::null_mut()}
+    }
+
+    fn val(epoch: usize, ptr: *mut ()) -> Self {
+        EpochPtr{epoch, ptr}
     }
 }
 
@@ -266,7 +289,7 @@ mod tests {
     fn size() {
         assert_eq!(mem::size_of::<AtomicU128>(), 16);
         assert_eq!(mem::size_of::<AtomicUsize>(), 8);
-        assert_eq!(mem::size_of::<EpochPtr>(), 16);
+        assert_eq!(mem::size_of::<AtomicEpochPtr>(), 16);
         assert_eq!(mem::size_of::<DataCell<*mut u8>>(), 16);
     }
 }
